75576dd6e17622e42850e554f5074d1655380c2c
Luke <166658343+TXSTXC@users.noreply.github.com>
2026-02-26 12:15:39 +0000

    SERVICES
    Updated the Process icons and layout, including a bespoke background image.
    Updated the 'Aftercare' section with a solid background.

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let myAge = "text";
+
+if(myAge >= 18){
+    console.log("You are an adult.");
+}
+else if( myAge < 18 && myAge > 0){
+    console.log("You are a child.");
+}
+else{
+    console.log("Mate, how have you got this so wrong?");
+}
